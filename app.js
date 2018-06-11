c177d80a6081650c317969118dc2f77b4a1cb99f
omeiregman <omeiregodsman@gmail.com>
2018-06-11 19:21:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+
+mongoose.connect(
+  'mongodb+srv://floratalk:wi7GPCEFo4Sop66h@cluster0-hb30n.mongodb.net/test');
+
+//mongoose.connect("mongodb://localhost:27017/floratalk");
+///mongoose.connect("mongodb://floratalk:floratalk1@ds147420.mlab.com:47420/floratalkdb");
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//FIX CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+      }
+  next();
+});
+
+
+//Routes handling Requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
+
+module.exports = app;
